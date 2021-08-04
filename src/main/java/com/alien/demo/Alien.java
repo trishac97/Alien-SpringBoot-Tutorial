9d641c71bbccedb32a006170c94570eef9f5bd0b
Trisha Chakraborty <trisha123chak@gmail.com>
2021-08-04 00:00:29 -0600

    Adding bean class

---
+++ src/main/java/com/alien/demo/Alien.java
@@ -0,0 +1,28 @@
+package com.alien.demo;
+
+public class Alien {
+	private int aid;
+	private String aname;
+	private String atech;
+	
+	
+	public int getAid() {
+		return aid;
+	}
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+	public String getAname() {
+		return aname;
+	}
+	public void setAname(String aname) {
+		this.aname = aname;
+	}
+	public String getAtech() {
+		return atech;
+	}
+	public void setAtech(String atech) {
+		this.atech = atech;
+	}
+	
+}
