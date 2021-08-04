2035a3c4b0111f5f8e931d8d4e6b91f5c4eb495b
Trisha Chakraborty <trisha123chak@gmail.com>
2021-08-03 18:44:32 -0600

    First Commit

---
+++ src/main/java/com/alien/demo/AlienSpringBootTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.alien.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlienSpringBootTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlienSpringBootTutorialApplication.class, args);
+	}
+
+}
